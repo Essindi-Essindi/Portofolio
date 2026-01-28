2e2073253a0609a08d3b6edb74027525ffb2eec0
Essindi Essindi <yvanstephanee@gmail.com>
2026-01-28 08:56:48 +0100

    Host

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/Portofolio/',
   server: {
     host: "::",
     port: 8080,
